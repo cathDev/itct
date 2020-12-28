96c36cd900cfd6a534b310725f44988d9f2b0801
cathDev <catherine.nganmo@interfacesa.com>
2020-12-28 18:49:21 +0100

    rendez vous update

--- src/app/layout/appointments/appointments.component.ts
+++ src/app/layout/appointments/appointments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import {ResourceService} from '../../shared/services/resource/resource.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-appointments',
@@ -12,9 +13,11 @@ export class AppointmentsComponent implements OnInit {
   url: string = "/client/appointment";
   appointments : any = [];
   search: string = "";
+  image : any;
 
   constructor(private formBuilder : FormBuilder,
-              private resourceService : ResourceService,) { }
+              private resourceService : ResourceService,
+              private sanitizer:DomSanitizer) { }
 
 
   ngOnInit() {
@@ -30,6 +33,22 @@ export class AppointmentsComponent implements OnInit {
         error => {
           console.log(error);
         });
+  }
+
+  public millisToDate(millis){
+    const dateObj = new Date(millis);
+    const month = dateObj.getMonth()+1;
+    const day = String(dateObj.getDate()).padStart(2, '0');
+    const year = dateObj.getFullYear();
+    const output = day + '-'+month+'-'+year;
+    return output;
+  }
+
+  public patientImage(img){
+    var image;
+    this.image = img;
+    image = 'data:image/png;base64,'+img;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(image);
   }
 
 
